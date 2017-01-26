bc921a5c63400463b2c00cdda0eeae4c35c3d214
stla <laurent_step@yahoo.fr>
2017-01-26 12:15:03 +0100

    new file

---
+++ src-commandline/MainReadComments.hs
@@ -0,0 +1,44 @@
+module Main where
+
+import ReadXLSX (readComments)
+import Options.Applicative
+import Data.Monoid ((<>))
+import qualified Data.ByteString.Lazy.Char8 as L
+import qualified Data.Text as T
+
+data Arguments = Arguments
+  { file :: String
+  , sheet :: String
+  , colnames :: Bool }
+
+getComments :: Arguments -> IO()
+getComments (Arguments file sheet colnames) =
+  do
+    json <- readComments file (T.pack sheet) colnames
+    L.putStrLn json
+
+run :: Parser Arguments
+run = Arguments
+     <$> strOption
+          ( metavar "FILE"
+         <> long "file"
+         <> short 'f'
+         <> help "XLSX file" )
+     <*> strOption
+          ( metavar "SHEET"
+         <> long "sheet"
+         <> short 's'
+         <> help "Sheet name" )
+     <*>  switch
+          ( long "header"
+         <> help "Whether the sheet has column headers" )
+
+
+main :: IO()
+main = execParser opts >>= getComments
+ where
+   opts = info (helper <*> run)
+     ( fullDesc
+    <> progDesc "Get comments of a XLSX sheet as a JSON string"
+    <> header "getXLSXcomments -- based on the xlsx Haskell library"
+    <> footer "Author: Stéphane Laurent" )
