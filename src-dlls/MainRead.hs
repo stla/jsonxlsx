a3ccae4b6c088e1f0e8e3df1d90fdf7b074293a7
stla <laurent_step@yahoo.fr>
2017-02-26 13:26:10 +0100

    dll

---
+++ src-dlls/MainRead.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ForeignFunctionInterface #-}
+module MainRead
+  where
+import Foreign
+import Foreign.C
+import ReadXLSX
+import Data.ByteString.Lazy.Internal (unpackChars)
+import Data.Text (splitOn, pack)
+
+foreign export ccall xlsx2jsonR :: Ptr CString -> Ptr CString -> Ptr CString -> Ptr CInt -> Ptr CString -> IO ()
+xlsx2jsonR :: Ptr CString -> Ptr CString -> Ptr CString -> Ptr CInt -> Ptr CString ->  IO ()
+xlsx2jsonR file sheetname what header result = do
+  -- peek arguments
+  file <- (>>=) (peek file) peekCString
+  sheetname <- (>>=) (peek sheetname) peekCString
+  what <- (>>=) (peek what) peekCString
+  header <- peek header
+  -- read
+  let keys = splitOn (pack ",") (pack what)
+  json <- case length keys of
+               1 -> sheetToJSON file (pack sheetname) (head keys) (header /= 0) Nothing Nothing
+               _ -> sheetToJSONlist file (pack sheetname) keys (header /= 0) Nothing Nothing
+  -- return
+  jsonC <- newCString $ unpackChars json
+  poke result $ jsonC
